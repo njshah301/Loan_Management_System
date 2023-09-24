2d61a54c87a96567ebe9ec82c7e4c2602f058dd2
njshah301 <201901026@daiict.ac.in>
2023-09-24 16:43:36 +0530

    spring security with JWT added

--- backend/loan_management_system/src/main/java/com/luma/LoanManagementSystemApplication.java
+++ backend/loan_management_system/src/main/java/com/luma/LoanManagementSystemApplication.java
@@ -2,13 +2,13 @@ package com.luma;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
-
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityFilterAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
 //import springfox.documentation.swagger2.annotations.EnableSwagger2;
 //@EnableSwagger2
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityFilterAutoConfiguration.class})
 public class LoanManagementSystemApplication {
 
 	@Bean
